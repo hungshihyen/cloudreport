a43f0122b48bc66d1c8779afd5d31a5711f04492
Andrew <flowingcash911@gmail.com>
2019-12-21 21:57:31 +0800

    iife

--- main.js
+++ main.js
@@ -1,72 +1,74 @@
-var router = new VueRouter({
-  // mode: 'history',
-  // base: '/',
-  routes: [
-    {
-      path: '',
-      component: Users(),
-      meta: { loginflag: true },
-      children: [
-        {
-          path: '/',
-          name: 'question',
-          component: Question(),
-          meta: { loginflag: true }
-        },
-        {
-          path: 'histories/:qid',
-          name: 'histories',
-          component: Histories(),
-          meta: { loginflag: true }
-        }
-      ]
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: Admin(),
-      meta: { loginflag: true, auth: true }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login(),
-      meta: { loginflag: true }
-    },
-    {
-      path: '*',
-      redirect: '/'
-    }
-  ]
-});
+(async () => {
+  var router = new VueRouter({
+    // mode: 'history',
+    // base: '/',
+    routes: [
+      {
+        path: '',
+        component: Users(),
+        meta: { loginflag: true },
+        children: [
+          {
+            path: '/',
+            name: 'question',
+            component: Question(),
+            meta: { loginflag: true }
+          },
+          {
+            path: 'histories/:qid',
+            name: 'histories',
+            component: Histories(),
+            meta: { loginflag: true }
+          }
+        ]
+      },
+      {
+        path: '/admin',
+        name: 'admin',
+        component: Admin(),
+        meta: { loginflag: true, auth: true }
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login(),
+        meta: { loginflag: true }
+      },
+      {
+        path: '*',
+        redirect: '/'
+      }
+    ]
+  });
 
-router.beforeEach(async (to, from, next) => {
-  await Vue.nextTick();
-  if (to.path !== '/login') {
-    to.meta.loginflag && app.login.loginId === ''
-      ? next({ path: '/login' })
-      : next();
-  } else {
-    next();
-  }
-});
+  router.beforeEach(async (to, from, next) => {
+    await Vue.nextTick();
+    if (to.path !== '/login') {
+      to.meta.loginflag && app.login.loginId === ''
+        ? next({ path: '/login' })
+        : next();
+    } else {
+      next();
+    }
+  });
 
-var app = new Vue({
-  el: '#app',
-  router,
-  data: {
-    login: {
-      loginName: '',
-      auth: 0,
-      loginId: ''
+  var app = new Vue({
+    el: '#app',
+    router,
+    data: {
+      login: {
+        loginName: '',
+        auth: 0,
+        loginId: ''
+      }
+    },
+    mounted() {
+      this.loginHandler();
+    },
+    methods: {
+      loginHandler() {
+        axios.get('./data.json').then(response => console.log(response.data));
+      }
     }
-  },
-  mounted() {
-    this.loginHandler();
-  },
-  methods: {
-    loginHandler() {
-      axios.get('./data.json').then(response => console.log(response.data));
-    }
-  }
-});
+  });
+})();
